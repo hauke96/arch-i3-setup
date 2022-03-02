eec43519fb5c5dd420075e457d33ee6ebff06b70
Hauke Stieler <mail@hauke-stieler.de>
2022-03-02 23:36:52 +0100

    Add own filebrowser mode to rofi

---
+++ configs/home/hauke/.config/rofi/files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$@" ]
+then
+	xdg-open $1
+	exit 0
+fi
+
+# Find all files:
+# - in max. 5 folder depth
+# - only files, no folder
+# - ignore all hidden folders as they contain a lot of uninteresting files
+# - ignore certain folders like git repos and node_module
+# - Just some formating: <file-name>  [<folder-name>]
+find \
+	-maxdepth 5 \
+	-type f \
+	| grep -v "\./\.[^/]*/" \
+	| grep -v "\.git/\|/node_modules/" \
+	| sed -E "s/^(.*\/)?(.*)$/\2  [\1]/g"
