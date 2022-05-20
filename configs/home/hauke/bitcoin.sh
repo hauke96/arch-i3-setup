894807019ac1eb102ca5b27b61058f82d3e16e35
Hauke Stieler <mail@hauke-stieler.de>
2022-05-20 12:22:03 +0200

    Update configs

--- configs/home/hauke/bitcoin.sh
+++ configs/home/hauke/bitcoin.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+set -e
+
 #cd ~/Apps
-sudo mount /dev/disk/by-uuid/03ea5b37-c934-411b-9785-be55f5f90509 /media/bitcoin
+#sudo mount /dev/disk/by-uuid/03ea5b37-c934-411b-9785-be55f5f90509 /media/bitcoin
 #sudo ./bitcoin-bin/bin/bitcoin-qt
+sudo mount /dev/sdf1 /mnt
 sudo bitcoin-qt
+sudo umount /mnt
