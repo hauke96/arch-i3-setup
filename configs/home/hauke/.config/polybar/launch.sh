064b511dada6e972f86dbab25eae1318139680e6
Hauke Stieler <mail@hauke-stieler.de>
2022-05-22 12:32:48 +0200

    Update starting of polybar for each monitor

--- configs/home/hauke/.config/polybar/launch.sh
+++ configs/home/hauke/.config/polybar/launch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	MONITOR=$m polybar --reload -c ~/.config/polybar/config bar &
+	MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
+	for m in $MONITORS; do
+		echo "Start polybar for monitor: $m"
+		MONITOR=$m polybar --reload -c ~/.config/polybar/config bar &
 	done
 else
+	echo "Start polybar on single monitor"
 	polybar --reload -c ~/.config/polybar/config bar &
 fi
