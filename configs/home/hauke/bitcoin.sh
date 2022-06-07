107ab2eba18d5a3bd3474273fc9dc75a381b134f
Hauke Stieler <mail@hauke-stieler.de>
2022-06-07 22:25:53 +0200

    Update bitcoin.sh script

--- configs/home/hauke/bitcoin.sh
+++ configs/home/hauke/bitcoin.sh
@@ -2,9 +2,21 @@
 
 set -e
 
-#cd ~/Apps
-#sudo mount /dev/disk/by-uuid/03ea5b37-c934-411b-9785-be55f5f90509 /media/bitcoin
-#sudo ./bitcoin-bin/bin/bitcoin-qt
-sudo mount /dev/sdf1 /mnt
-sudo bitcoin-qt
-sudo umount /mnt
+if [[ "$EUID" = 0 ]]
+then
+	#cd ~/Apps
+
+	echo "Mounting disk"
+	mount /dev/disk/by-uuid/a4e8c16e-25c4-416c-93b7-9309262c98de /mnt
+	#sudo mount /dev/sdf1 /mnt
+
+	echo "Starting bitcoin client"
+	#sudo ./bitcoin-bin/bin/bitcoin-qt
+	bitcoin-qt
+
+	echo "Unmounting disk"
+	umount /mnt
+else
+	>&2 echo "Permission denied: This script needs root permissions."
+	exit 1
+fi
