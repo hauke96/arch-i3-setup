3414e0becb74921220fae9e2029e01a07851f453
Hauke Stieler <mail@hauke-stieler.de>
2023-02-10 15:07:27 +0100

    Update rofi file runner

--- configs/home/hauke/.config/rofi/runner.sh
+++ configs/home/hauke/.config/rofi/runner.sh
@@ -3,16 +3,19 @@
 CACHE="$HOME/.cache/rofi.filecache"
 
 # Find all files:
-# - in max. 5 folder depth
+# - in max. 7 folder depth
 # - only files, no folder
-# - ignore all hidden folders as they contain a lot of uninteresting files
+# - ignore all hidden things (they often contain a lot of uninteresting files)
 # - ignore certain folders like git repos and node_module
 # - Just some formating: <file-name>  [<folder-name>]
-find \
+find $HOME \
 	-maxdepth 5 \
 	-type f \
-	| grep -v "\./\.[^/]*/" \
-	| grep -v "\.git/\|/node_modules/" \
-	| grep -Pv "\.(toc|snm|nav|out|aux|synctex.gz)$" \
+	-follow \
+	| grep -v "/\.[^/]*/" \
+	| grep -Pv "/(Android/Sdk|bin|cache|CMakeFiles|fdroiddata|go/pkg|GoPro|lustre|okular|node_modules|obj|out|tmp|zfs)/" \
+	| grep -Pv "/xyz(-(raster-tiles|vector-tiles))?/" \
+	| grep -Pv "\.(a|aux|bbl|blg|dvi|log|map|nav|o|out|pygtex|rom|snm|sty|synctex.gz|toc|tmp|vrb)$" \
 	| sed -E "s/^(.*\/)?(.*)$/\2  [\1]/g" \
 	> "$CACHE"
+# Not necessary because all hidden stuff is ignored: | grep -Pv "\.(android|cache|git|idea|metadata|run|tmp)" \
