6406ddcee299b6b29b471ffa53bf587eacf49059
Hauke Stieler <mail@hauke-stieler.de>
2023-02-19 12:03:13 +0100

    Update theme and use PNG

--- grub/install.sh
+++ grub/install.sh
@@ -16,13 +16,11 @@ fi
 THEME_DIR="/boot/grub/themes"
 THEME_NAME=Xenlism-Arch
 
-# If folder exists -> nothing to do here
+# If folder exists -> remove it to re-install the theme
 if [[ -d ${THEME_DIR}/${THEME_NAME} ]]
 then
-#	echo "GRUB theming already installed. Abort."
 	echo "GRUB theme already installed -> clean up"
 	rm -rf "${THEME_DIR}/${THEME_NAME}"
-#	exit 0
 fi
 
 # Create themes directory if not exists
@@ -31,7 +29,7 @@ mkdir -p "${THEME_DIR}/${THEME_NAME}"
 
 # Copy theme
 echo "Copy theme files"
-cp -a ${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
+cp -a ${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME} || true
 
 # Set theme
 # Backup grub config
