b8bc6d7a303c4a90c299c75ac8e56075b7ebc026
Hauke Stieler <mail@hauke-stieler.de>
2023-04-05 19:35:20 +0200

    Revise shutdown.sh

--- configs/home/hauke/shutdown.sh
+++ configs/home/hauke/shutdown.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -x
+	
+if [ "$(id -u)" != 0 ]
+then
+	echo "This must be run as root!"
+	exit 1
+fi
 
 echo "Start shutdown: $(date)"
 
@@ -16,16 +22,17 @@ killall webstorm
 killall spotify
 
 # Update packages
-sudo pacman -Syu
+#sudo -u hauke yay -Syyu --aur --noconfirm
+sudo -u hauke yay -Syyu --sudoloop --noconfirm
 
 # Stop akonadi as this also might inconsistend backups
 akonadictl stop
 
 # Create backups
 ./backup.sh /media/backup-home /home/hauke
-sudo ./backup.sh /media/backup-data/system/current "/etc /var /boot /opt"
+./backup.sh /media/backup-data/system/current "/etc /var /boot /opt"
 ./backup.sh /media/backup-data/data /media/data
 
 # Actually shutdown
 echo "End shutdown: $(date)"
-shutdown -h now
+sudo shutdown -h now
