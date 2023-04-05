a7aa918c7e9db2d3ac38b1e2f6d201452f28b1eb
Hauke Stieler <mail@hauke-stieler.de>
2023-04-05 19:35:43 +0200

    Add autostart script of i3

---
+++ configs/home/hauke/.config/i3/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Wait one second to ensure that all layout restore windows from i3 are up.
+# This is necessary because an already opened application is not catched by a
+# layout resore window, which will just stay open.
+sleep 1
+
+(telegram-desktop) &
+(signal-desktop) &
+(spotify) &
+(firefox) &
+(kontact) &
+(keepassxc) &
