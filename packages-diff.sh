c7334e42cf1584fba7c42d06d51a95d12c1f9ae3
Hauke Stieler <mail@hauke-stieler.de>
2023-11-04 09:01:58 +0100

    Update package list and package-diff.sh

--- packages-diff.sh
+++ packages-diff.sh
@@ -5,4 +5,8 @@ sudo pacman -Qe | awk '{print $1}' | sort > packages-installed.txt
 
 cat packages/aur/* packages/pacman/* | grep -v "^#\|^\s*$" | sort > packages-setup.txt
 
-git diff --no-index packages-installed.txt packages-setup.txt
+echo "Red  : Installed but not in repo."
+echo "Green: In repo but not installed."
+echo
+
+git --no-pager diff --no-index packages-installed.txt packages-setup.txt
